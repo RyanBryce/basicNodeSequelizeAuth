8041fa5051057a6ad9c7e6896258ffc04f6e2f30
Ryan Bryce <ryan.bryce1992@gmail.com>
2018-01-12 14:00:28 -0700

    adding file structure

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var path = require('path');
+module.exports = function(app){
+//render html
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors');
+var session = require('express-session');
+var db = require('./models')
+
+var app = express()
+app.use(cors());
+app.use(express.static('./public'))
+
+var PORT = process.env.PORT
+
+app.use(session({
+  secret: process.env.SESSIONSECRET || config.sessionSecret,
+  resave: false,
+  saveUninitialized: true
+}));
+
+//middleware for setting up a user object when anyone first come to the appplication
+function userSetup(req, res, next){
+  if(!req.session.user){
+    req.session.user = {}
+    req.session.user.loggedIn = false;
+  }
+  next()
+}
+
+//using middlewhere acrossed the entire application before any route gets hit.
+app.use(userSetup)
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+require('./routes/htmlRoutes')(app)
+require('./routes/apiRoutes')(app)
+
+
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
